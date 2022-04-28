89bbc955cddfbff7617f4a5a05662048c8ec5bd0
EthanDonaldson <93975627+EthanDonaldson@users.noreply.github.com>
2022-04-28 18:20:37 +0100

    Update connections.php

--- Teamproject/connections.php
+++ Teamproject/connections.php
@@ -4,9 +4,21 @@ $dbhost = "localhost";
 $dbuser = "root";
 $dbpass = "";
 $dbname = "login_db";
+$host = "localhost"; /* Host name */
+$user = "root"; /* User */
+$password = ""; /* Password */
+$dbname1 = "upload_db"; /* Database name */
+
 
 if(!$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname))
 {
 
 	die("failed to connect!");
+}
+
+
+$con1 = mysqli_connect($host, $user, $password,$dbname1);
+// Check connection
+if (!$con1) {
+  die("Connection failed: " . mysqli_connect_error());
 }
